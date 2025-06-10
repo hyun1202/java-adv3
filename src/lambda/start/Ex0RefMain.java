1da466eb89ba563044ee64d43103c90874d7a503
hyun <apfhd0257@naver.com>
2025-06-10 10:38:46 +0900

    람다가 필요한 이유

---
+++ src/lambda/start/Ex0RefMain.java
@@ -0,0 +1,22 @@
+package lambda.start;
+
+public class Ex0RefMain {
+    public static void helloJava() {
+        hello("Java");
+    }
+
+    public static void helloSpring() {
+        hello("Spring");
+    }
+
+    public static void hello(String str) {
+        System.out.println("프로그램 시작");
+        System.out.println("Hello " + str);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        helloJava();
+        helloSpring();
+    }
+}
